f9901a506a1afb8ff433413c04b7e6e9a22844b7
username <younguk137@naver.com>
2025-09-13 22:56:06 +0900

    solved

---
+++ 4153.py
@@ -0,0 +1,9 @@
+while True:
+    A,B,C = map(int,input().split())
+    if (A,B,C) == (0, 0, 0) :
+        break
+    else:
+        if (A*A ==B*B+C*C) or (B*B==A*A+C*C) or (C*C==A*A+B*B) :
+            print('right')
+        else:
+            print('wrong')
