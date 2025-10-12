87e69a377b58d58469cfa08f11288782b62a38c1
username <younguk137@naver.com>
2025-10-12 15:13:13 +0900

    1012

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-N,M = map(int, input().split())
-L = list(map(int,input().split()))
-L.sort()
-import itertools
-for i in itertools.combinations_with_replacement(L,M):
-    for j in i:
-        print(j,end=' ')
-    print('')
