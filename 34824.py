a0312a4036f1b51a6ed1d656d1e24859e4386e51
username <younguk137@naver.com>
2025-12-02 13:25:29 +0900

    1202

---
+++ 34824.py
@@ -0,0 +1,11 @@
+N = int(input())
+for i in range(N):
+    S =input()
+    if S =='yonsei':
+        print('Yonsei Won!')
+        break
+    elif S=='korea':
+        print('Yonsei Lost...')
+        break
+    else:
+        pass
